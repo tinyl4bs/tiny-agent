edf2e41c06e63a37147bbdf81074a0eae99a751c
anzanipaolodev@gmail.com <anzanipaolodev@gmail.com>
2024-12-03 16:22:36 +0100

    Adds AvgPriceTool and 24hrTool

--- src/tools/binance-api.ts
+++ src/tools/binance-api.ts
@@ -24,6 +24,32 @@ const getListOfSymbols = async () => {
     return data.symbols.map((symbol: any) => symbol.symbol);
 };
 
+export const ticker24hrTool: ITool = {
+    name: 'TICKER_24HR',
+    similies: ['TICKER_24HR', 'BINANCE_TICKER_24HR', 'TOKEN_24HR'],
+    description: 'Get the 24hr ticker data for a symbol from Binance public API',
+    inputFormat: '<string>',
+    inputExamples: ['BTCUSDT', 'ETHUSDT', 'SOLUSDT'],
+    execute: async (query: string) => {
+        const response = await fetch(`${Public.TICKER_24HR}?symbol=${query}`);
+        const data = await response.json();
+        return JSON.stringify(data);
+    },
+};
+
+export const tickerAvgPriceTool: ITool = {
+    name: 'TICKER_AVG_PRICE',
+    similies: ['TICKER_AVG_PRICE', 'BINANCE_TICKER_AVG_PRICE', 'TOKEN_AVG_PRICE'],
+    description: 'Get the average price of a symbol from Binance public API',
+    inputFormat: '<string>',
+    inputExamples: ['BTCUSDT', 'ETHUSDT', 'SOLUSDT'],
+    execute: async (query: string) => {
+        const response = await fetch(`${Public.AVG_PRICE}?symbol=${query}`);
+        const data = await response.json();
+        return JSON.stringify(data);
+    },
+};
+
 export const tickerPriceTool: ITool = {
     name: 'TICKER_PRICE',
     similies: ['TICKER_PRICE', 'BINANCE_TICKER_PRICE', 'TOKEN_PRICE'],
